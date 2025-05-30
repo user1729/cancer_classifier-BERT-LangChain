4903e3d262c58ae454b221ae65f7e47fb259894c
Rahul R <rahul91indian@gmail.com>
2025-05-30 12:52:46 +0530

    Update run_docker.sh

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -4,7 +4,7 @@
 source init-docker.sh
 
 docker run --runtime=nvidia \
-  --mount type=bind,source=${DATA_VOLM},target=/home/developer/ \
+  --mount type=bind,source=${PWD},target=/home/developer/ \
   -w /home/developer/ \
   --rm --ipc=host -it \
   --name research-dev \
